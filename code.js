91b0912dbb498f23830d48ea3cd0d49daf084f9e
Kodoka <10721764+Kodoka@users.noreply.github.com>
2025-04-22 12:23:10 -0600

    Done hopefully

--- code.js
+++ code.js
@@ -1,8 +1,22 @@
+function factorial(n)
+{
+    // 0! == 1
+    var factorials = [1];
+
+    // Using 0! == 1, we fill factorials up to n.
+    for(var i = 1; i <= n; i++)
+    {
+        factorials[i] = i * factorials[i - 1];
+    }
+
+    return factorials[n];
+}
+
 function e(n)
 {
     // 0! / 1 == 1, so eSum starts at 1, as n == 0 is minimum.
     var eSum = 1;
-    // 0! is 1.
+    // 0! == 1.
     var factorials = [1];
 
     // Using 0! == 1, we fill factorials up to n, so we aren't recomputing
@@ -16,5 +30,3 @@ function e(n)
 
     return eSum;
 }
-
-console.log(JSON.stringify(e(100)));
--- code.test.js
+++ code.test.js
@@ -2,5 +2,27 @@ const fs = require('fs');
 const assert = require('assert');
 eval(fs.readFileSync('code.js')+'');
 
+// Testing base cases.
+assert(factorial(0) === 1);
+assert(factorial(1) === 1);
+
+// Included test.
 assert(factorial(10) === 3628800);
+
+// Testing larger values.
+assert(factorial(20) === 2432902008176640000);
+
+// Testing base cases.
+assert(e(0) === 1);
+assert(e(1) === 2);
+
+// Included test.
 assert(e(10) === 2.7182818011463845);
+
+// Accuracy of e should increase as n increases, resulting
+// in a slightly larger value.
+assert(e(15) > e(10));
+assert(e(16) > e(15));
+// From my testing e(17) is the most accurate the code
+// gets in it's current form.
+assert(e(17) > e(16));
