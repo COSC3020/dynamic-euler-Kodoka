551b15600940a65dc40e6df6b7825439098f5cd1
Kodoka <10721764+Kodoka@users.noreply.github.com>
2025-04-22 08:55:10 -0600

    Update code.js
    Didn't look at test code before I got coding. Need to add factorial back in. Pushing to work on laptop.

--- code.js
+++ code.js
@@ -1,9 +1,20 @@
-function factorial(n) {
-    if(n === 0) return 1;
-    else return n * factorial(n - 1);
+function e(n)
+{
+    // 0! / 1 == 1, so eSum starts at 1, as n == 0 is minimum.
+    var eSum = 1;
+    // 0! is 1.
+    var factorials = [1];
+
+    // Using 0! == 1, we fill factorials up to n, so we aren't recomputing
+    // factorials repeatedly. i starts at 1, 1 * 1 = 1, then add 1 / i ! to
+    // eSum.
+    for(var i = 1; i <= n; i++)
+    {
+        factorials[i] = i * factorials[i - 1];
+        eSum += (1 / factorials[i]);
+    }
+
+    return eSum;
 }
 
-function e(n) {
-    if(n === 0) return 1;
-    else return 1.0 / factorial(n) + e(n - 1);
-}
+console.log(JSON.stringify(e(100)));
